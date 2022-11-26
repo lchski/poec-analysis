3866b1e5dbba7f8be135213f479b7e15b1fae4c7
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-11-26 13:01:37 -0500

    move debug code into standalone file

--- load.R
+++ load.R
@@ -233,10 +233,6 @@ lines <- lines %>% left_join(
 
 
 
-# for debuggnig, confirm there are no duplicate names (typos, title inconsistencies etc)
-lines %>%
-  count(speaker_standardized) %>%
-  View("speaker_standardized_count")
 
 testimony <- lines %>%
   filter(
@@ -273,17 +269,6 @@ testimony %>%
   group_by(interjection_id) %>%
   summarize(text_clean = paste0(text_clean, collapse = " "))
 
-# for debugging, find non-speech lines followed by non-speaker_start lines (after a heading, or time marker, etc, the speaker should always be identified)
-unexpected_line_type_following_non_speech <- testimony %>%
-  mutate(next_line_type = lead(line_type), next_text = lead(text)) %>%
-  filter(! line_type %in% c("speaker_start", "testimony") & lead(line_type) != "speaker_start") %>%
-  select(-transcript_line_number, -text_clean, -speaker, -speaker_standardized)
-
-unexpected_line_type_following_non_speech %>%
-  filter(next_line_type == "testimony")
-
-# When non-speech is followed by non-speech - all good.
-# When non-speech is followed immediately by testimony - likely problem. (Usually, multi-line section_header. Sometimes, speaker ID is missing from original transcript.)
 
 # TODO idea: speaker type, counsel, lawyer, admin, witness (based on TOC entries?)
 
---
+++ scripts/debugging.R
@@ -0,0 +1,37 @@
+# ==== Verifying proceedings_end is always on last page ====
+end_lines <- lines %>%
+  filter(line_type == "proceedings_end") %>%
+  select(day, end_page = page, end_line = line)
+
+end_lines %>%
+  left_join(
+    lines %>%
+      group_by(day) %>%
+      summarize(max_page = max(page))
+  ) %>%
+  mutate(end_is_max = end_page == max_page) %>%
+  count(end_is_max)
+
+rm(end_lines)
+
+
+
+# ==== Confirm there are no duplicate names (typos, title inconsistencies etc) ====
+lines %>%
+  count(speaker_standardized) %>%
+  View("speaker_standardized_count")
+
+
+
+# ==== Find non-speech lines followed by non-speaker_start lines ====
+# After a heading, or time marker, etc, the speaker should always be identified
+unexpected_line_type_following_non_speech <- testimony %>%
+  mutate(next_line_type = lead(line_type), next_text = lead(text)) %>%
+  filter(! line_type %in% c("speaker_start", "testimony") & lead(line_type) != "speaker_start") %>%
+  select(-transcript_line_number, -text_clean, -speaker, -speaker_standardized)
+
+unexpected_line_type_following_non_speech %>%
+  filter(next_line_type == "testimony")
+
+# When non-speech is followed by non-speech - all good.
+# When non-speech is followed immediately by testimony - likely problem. (Usually, multi-line section_header. Sometimes, speaker ID is missing from original transcript.)
